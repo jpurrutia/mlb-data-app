70bf69bf7852332efab586ae90d880c20c9314c5
jpurrutia <jpurrutia95@gmail.com>
2024-09-10 13:50:34 -0500

    updated tables with schema ddl

--- sql_queries/schema.sql
+++ sql_queries/schema.sql
@@ -1,7 +1,10 @@
-CREATE TABLE mlb.schedule (
-	id uuid PRIMARY KEY NOT NULL,
+CREATE TABLE mlb.raw_schedule (
+	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 	schedule_date timestamp NOT NULL,
-	games_payload jsonb -- could be NULL IF there ARE NO games that day
+	schedule_payload jsonb NULL,
+	created_at timestamptz DEFAULT NOW(),
+	updated_at timestamptz DEFAULT NOW(),
+	UNIQUE (schedule_date) 
 );
 
 CREATE TABLE mlb.games (
@@ -10,14 +13,20 @@ CREATE TABLE mlb.games (
 	game_payload jsonb NOT NULL
 );
 
-CREATE TABLE mlb.pbp (
-	id uuid PRIMARY KEY NOT NULL,
-	mlbam_game_id integer NOT NULL,
-	pbp_payload jsonb NOT NULL
+CREATE TABLE mlb.raw_pbp (
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	mlbam_game_date date NOT NULL,
+	mlbam_game_id int4 NOT NULL,
+	pbp_payload jsonb NOT NULL,
+	created_at timestamptz DEFAULT NOW(),
+	updated_at timestamptz DEFAULT NOW(),
+	UNIQUE (mlbam_game_id)
+	-- should the unique be a concat of game, date?
 );
 
+
 CREATE TABLE mlb.raw_game_lineups (
-	-- might need to see if this is scalable
+	-- might need to see if this is scalable ID creation
     id UUID,
 	schedule_date timestamp NOT NULL,
 	mlbam_game_id integer NOT NULL,
@@ -30,3 +39,8 @@ CREATE TABLE mlb.raw_game_lineups (
 	updated_at timestamptz DEFAULT NOW(),
 	UNIQUE (id)
 );
+-- Mock Insert Record
+/*
+INSERT INTO mlb.raw_game_lineups (schedule_date, mlbam_game_id, mlbam_team_id, lineup, probable_pitcher, bullpen, home_flag, away_flag, lineup_set, created_at, updated_at)
+VALUES ('2024-08-24', 745541, 117, ARRAY[656941, 607208, 547180, 664761, 679032, 624641, 669016, 669720, 596117], 681293, ARRAY[650556, 669854, 686613, 657571, 623352, 579328, 593576, 672391, 605463, 664285, 434378], FALSE, TRUE, TRUE, NOW(), NOW());
+*/
