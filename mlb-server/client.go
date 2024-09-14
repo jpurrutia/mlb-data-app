3d316ebef49b515b7d94c342472ffc3da5bd9a1b
jpurrutia <jpurrutia95@gmail.com>
2024-09-13 23:56:18 -0500

    comment out client

--- mlb-server/client.go
+++ mlb-server/client.go
@@ -1,25 +1,25 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"net/http"
-	"time"
-)
+// import (
+// 	"fmt"
+// 	"net/http"
+// 	"time"
+// )
 
-func main() {
-	client := http.Client{
-		Timeout: time.Second * 2,
-	}
+// func main() {
+// 	client := http.Client{
+// 		Timeout: time.Second * 2,
+// 	}
 
-	req, err := http.NewRequest("GET", "http://localhost:8080/users/", nil)
-	if err != nil {
-		panic(err)
-	}
-	body, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
+// 	req, err := http.NewRequest("GET", "http://localhost:8080/users/", nil)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	body, err := client.Do(req)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	fmt.Println(body)
+// 	fmt.Println(body)
 
-}
+// }
