2336ff6f0b50ea64aa752891ca81fae29733c922
jpurrutia <jpurrutia95@gmail.com>
2024-09-27 23:54:29 -0400

    update values

--- sql_queries/mlb_curate_lineup_values.sql
+++ sql_queries/mlb_curate_lineup_values.sql
@@ -14,41 +14,41 @@ WITH flattened_game_lineups AS (
 	ON g.game_id = l.mlbam_game_id
 ),
 home_batter_game_runs_created AS (
-SELECT l.game_id
-	,l.home_batters
-	,CAST(l.home_team_id AS integer) AS home_team_id
-	,'home' AS home_away
-	,c.technical_rc
-FROM flattened_game_lineups l
-JOIN mlb.curated_events_runs_created c
-ON l.game_id = c.mlbam_game_id 
-AND CAST(l.home_batters AS integer) = CAST(c.player_id AS integer)
+	SELECT l.game_id
+		,l.home_batters
+		,CAST(l.home_team_id AS integer) AS home_team_id
+		,'home' AS home_away
+		,c.technical_rc
+	FROM flattened_game_lineups l
+	JOIN mlb.curated_events_runs_created c
+	ON l.game_id = c.mlbam_game_id 
+	AND CAST(l.home_batters AS integer) = CAST(c.player_id AS integer)
 ),
 away_batter_game_runs_created AS (
-SELECT l.game_id
-	,l.away_batters
-	,CAST(l.away_team_id AS integer) AS away_team_id
-	,'away' AS home_away
-	,c.technical_rc
-FROM flattened_game_lineups l
-JOIN mlb.curated_events_runs_created c
-ON l.game_id = c.mlbam_game_id 
-AND CAST(l.away_batters AS integer) = CAST(c.player_id AS integer)
+	SELECT l.game_id
+		,l.away_batters
+		,CAST(l.away_team_id AS integer) AS away_team_id
+		,'away' AS home_away
+		,c.technical_rc
+	FROM flattened_game_lineups l
+	JOIN mlb.curated_events_runs_created c
+	ON l.game_id = c.mlbam_game_id 
+	AND CAST(l.away_batters AS integer) = CAST(c.player_id AS integer)
 ),
 game_batter_lineup_values AS (
-SELECT game_id
-	,home_team_id AS team_id	
-	,home_batters AS batters
-	,home_away
-	,technical_rc
-FROM home_batter_game_runs_created
-UNION ALL
-SELECT game_id
-	,away_team_id AS team_id
-	,away_batters AS batters
-	,home_away
-	,technical_rc
-FROM away_batter_game_runs_created
+	SELECT game_id
+		,home_team_id AS team_id	
+		,home_batters AS batters
+		,home_away
+		,technical_rc
+	FROM home_batter_game_runs_created
+	UNION ALL
+	SELECT game_id
+		,away_team_id AS team_id
+		,away_batters AS batters
+		,home_away
+		,technical_rc
+	FROM away_batter_game_runs_created
 )
 SELECT 
 	team_id
