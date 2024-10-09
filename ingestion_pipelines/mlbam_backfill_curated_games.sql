e7a7de97b0bf17f09549fd3a86879610f5b70c33
jpurrutia <jpurrutia95@gmail.com>
2024-10-09 16:57:57 -0500

    added a new version of the query because I want to deal with postponed games and overwrite those values

--- ingestion_pipelines/mlbam_backfill_curated_games.sql
+++ ingestion_pipelines/mlbam_backfill_curated_games.sql
@@ -117,3 +117,73 @@ FROM flatten_game_duplicates
 WHERE row_num > 1
 ORDER BY game_date asc
 
+
+
+---- THIS IS A NEW VERSION THAT I AM TESTING BECAUSE I WANT TO GET THE LATEST GAME FOR ALL the postponed games and overwrite values
+
+WITH dates_payload AS (
+	SELECT jsonb_array_elements(schedule_payload->'dates') AS dates_payload
+	FROM mlb.raw_schedule
+),
+flattened_games_payload AS (
+	SELECT dates_payload->>'date' AS date
+		,jsonb_array_elements(dates_payload->'games') AS games
+	FROM dates_payload
+),
+latest_game_info AS (
+SELECT
+	(games->>'gamePk')::integer AS game_id
+	,CAST(date AS DATE) AS date
+	,(games->>'gameGuid')::uuid AS game_guid
+	,games->>'season' AS season
+	,games->>'gameType' AS game_type
+	--,CASE
+	--	WHEN games->'status'->>'statusCode' = 'F' AS game_status THEN
+	--	WHEN games->'status'->>'statusCode' = 'F' AS game_status THEN
+	--END AS games_
+	,games->'status'->>'codedGameState' AS game_state_code
+	,games->'status'->>'detailedState' AS game_state
+	,(games->>'gameNumber')::integer AS game_number
+	,(games->>'dayNight')::text AS day_night
+	,(CASE
+		WHEN games->>'doubleHeader' = 'N' THEN 0
+		WHEN games->>'doubleHeader' = 'Y' THEN 1
+		ELSE NULL
+	END)::boolean AS double_header_flag
+	,(games->'teams'->'away'->'team'->>'id')::integer AS away_team_id	
+	-- games->'team's->'away'->'team'->'name' AS away_team_name
+	,(games->'teams'->'away'->>'score')::integer AS away_score
+	,(games->'teams'->'away'->'leagueRecord'->>'wins')::integer AS away_team_wins
+	,(games->'teams'->'away'->'leagueRecord'->>'losses')::integer AS away_team_losses
+	,(games->'teams'->'home'->'team'->>'id')::integer AS home_team_id
+	-- games->'team's->'home'->'team'->'name' AS home_team_name
+	,(games->'teams'->'home'->'score')::integer AS home_score
+	,(games->'teams'->'home'->'leagueRecord'->>'wins')::integer AS home_team_wins
+	,(games->'teams'->'home'->'leagueRecord'->>'losses')::integer AS home_team_losses
+	,(games->'venue'->'id')::integer AS venue_id
+FROM flattened_games_payload 
+WHERE date IN ('1999-06-07', '1999-06-08')
+ORDER BY game_id ASC
+)
+SELECT
+game_id
+,MAX(date) AS date
+,game_guid 
+,MAX(season) AS seasion
+,MAX(game_type) AS game_type
+,MAX(game_state_code) AS game_state_code
+,MIN(game_state) AS game_state
+,MAX(game_number) AS game_number
+,MAX(day_night) AS day_night
+,MAX(double_header_flag)
+,MAX(away_team_id)
+,MAX(away_score)
+,MAX(away_team_wins)
+,MAX(away_team_losses)
+,MAX(home_team_id)
+,MAX(home_score)
+,MAX(home_team_wins)
+,MAX(home_team_losses)
+,MAX(venue_id)
+FROM latest_game_info
+GROUP BY game_id, game_guid
