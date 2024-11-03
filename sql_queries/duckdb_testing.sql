2e8ddba095028d6b3deb09dbb10da0fd2973a997
jpurrutia <jpurrutia95@gmail.com>
2024-11-03 16:57:28 -0600

    play_event_info query updated

--- sql_queries/duckdb_testing.sql
+++ sql_queries/duckdb_testing.sql
@@ -45,49 +45,41 @@ D WITH play_events_cte AS (
 ‣ WHERE description = 'Foul'
 · ;
 
---- Offensive Substituiton
-WITH play_events_cte AS (
-    SELECT
-        unnest(liveData.plays.allPlays).playEvents as play_events
-    FROM
-        post_game_data
-),
-unnested_event_details_cte AS (
-    SELECT
-        unnest(play_events) AS unnested_event_details
-    FROM
-        play_events_cte
-)
-    SELECT
-        unnested_event_details.details.description as description
-    FROM
-        unnested_event_details_cte
-    WHERE description like '%Offensive%';
 
-
--- Offensive Sub Postgres Version
+-- Offensive Sub Duck DB Version
 WITH play_events AS (
-SELECT
-    (unnest(liveData.plays.allPlays, recursive := true)) AS play_events
-FROM
-    post_game_data
-),
-event_details AS (
     SELECT
-        unnest(playEvents).details.description AS description
-    FROM play_events
-)
-SELECT description
-FROM event_details
-WHERE description LIKE '%Offensive Substitution%';
+      (unnest(liveData.plays.allPlays, recursive := true)) AS play_events
+    FROM
+      post_game_data
+  ),
+  event_details AS (
+      SELECT
+          unnest(playEvents).details.description AS description
+      FROM play_events
+  )
+  SELECT description
+  FROM event_details
+  WHERE description LIKE '%Offensive Substitution%';
 
 -- PostgreSQL
-SELECT 
-	jsonb_array_elements(
+WITH play_event_info AS (
+SELECT *
+	,jsonb_array_elements(pbp_payload->'liveData'->'plays'->'allPlays')->'matchup'->'batter'->'id' AS batter_id
+	,jsonb_array_elements(pbp_payload->'liveData'->'plays'->'allPlays')->'matchup'->'batter'->>'fullName' AS batter_fullname
+	,jsonb_array_elements(pbp_payload->'liveData'->'plays'->'allPlays')->'matchup'->'batter' AS batter_info
+	,jsonb_array_elements(
 		(jsonb_array_elements(
-			pbp_payload->'liveData'->'plays'->'allPlays'))->'playEvents')->'details'->>'description' AS play_events
+			pbp_payload->'liveData'->'plays'->'allPlays'))->'playEvents')->'details'->>'description' AS play_event_description
 FROM
-    mlb.raw_pbp rp LIMIT 100;
+    mlb.raw_pbp rp WHERE mlbam_game_id = 775294
+)
+SELECT 
+	*
+	play_event_d
+FROM play_event_info 
+WHERE play_event_description LIKE '%Offensive%'
+    
     
 
 
